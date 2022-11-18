393ebef6d8a27d496197b18954636e44bfda9e2e
Monika <moncobra@comcast.net>
2022-11-18 14:55:36 -0800

    added get character, get character by id, and check error funcion in fetch utils

---
+++ fetch-utils.js
@@ -0,0 +1,18 @@
+const SUPABASE_URL = 'https://giwptggnnkyngbvntavn.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdpd3B0Z2dubmt5bmdidm50YXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDg1MDUsImV4cCI6MTk4MzY4NDUwNX0.IpqZ1fOasMiRTSsQIkHj5BOCwSYQSi4zxsS9Hhx76x0';
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getCharacter() {
+    const response = await client.from('anime').select();
+    return response;
+}
+
+export async function getCharacter(id) {
+    const response = await client.from('anime').select().match({ id: id }).single();
+    return checkError(response);
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
